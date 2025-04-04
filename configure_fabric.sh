e28d8a93984f3d69ec385d796e71001b86fae116
kkayhan <kayhankorhan@gmail.com>
2025-04-04 17:36:38 +0200

    mini_fix

--- configure_fabric.sh
+++ configure_fabric.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --rm -it -v ./ansible-srlinux:/ansible -v ~/.ssh:/~/.ssh -v /etc/hosts:/etc/hosts -w /ansible ghcr.io/nokia/srlinux-ansible-collection/2.17.1/pypy3.10 ansible-playbook -i inventory.ini site.yml
+docker run --rm -it \
+  -v "$(pwd)/ansible-srlinux:/ansible" \
+  -v ~/.ssh:/root/.ssh \
+  -v /etc/hosts:/etc/hosts \
+  -w /ansible \
+  ghcr.io/nokia/srlinux-ansible-collection/2.17.1/pypy3.10 ansible-playbook -i inventory.ini site.yml "$@"
