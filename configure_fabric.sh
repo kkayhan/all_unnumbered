7e1a2c24517862110bf36e684691156e564eea1c
kkayhan <kayhankorhan@gmail.com>
2025-04-04 17:20:52 +0200

    quick fix on sh

--- configure_fabric.sh
+++ configure_fabric.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v /root/all_unnumbered/ansible-srlinux:/ansible -v ~/.ssh:/root/.ssh -v /etc/hosts:/etc/hosts -w /ansible ghcr.io/nokia/srlinux-ansible-collection/2.17.1/pypy3.10 ansible-playbook -i inventory.ini site.yml
+docker run --rm -it -v ./ansible-srlinux:/ansible -v ~/.ssh:/~/.ssh -v /etc/hosts:/etc/hosts -w /ansible ghcr.io/nokia/srlinux-ansible-collection/2.17.1/pypy3.10 ansible-playbook -i inventory.ini site.yml
